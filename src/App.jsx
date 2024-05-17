551b538d6d371e1656722fd4b592b1224c2aca34
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-17 16:10:53 +0000

    homeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomeScreen from './Screens/Home/HomeScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
-    </>
+    <div>
+      <HomeScreen />
+    </div>
   )
 }
 
