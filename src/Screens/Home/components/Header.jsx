a23f44e48d8754108e6b4a7889d616ff6d1bda85
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-18 07:06:31 +0000

    nav-btns

--- src/Screens/Home/components/Header.jsx
+++ src/Screens/Home/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <button className='btn btn-primary btn-sm md:btn-md'>+ New Code Tip</button>
+      <h2 className='font-bold text-sm md:text-2xl'>Top 20 Code Tips</h2>
+    </div>
   )
 }
 
