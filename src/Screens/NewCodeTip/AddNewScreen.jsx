ff5d7e68f1ff9cfb70119a55168c955ebe7db884
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-19 17:28:26 +0000

    codetip write section

--- src/Screens/NewCodeTip/AddNewScreen.jsx
+++ src/Screens/NewCodeTip/AddNewScreen.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from '../Home/components/Header'
 
 const AddNewScreen = () => {
   return (
-    <div>AddNewScreen</div>
+    <div>
+      <Header/>
+
+      <button className='btn mt-7'>Back</button>
+      <h2 className='font-bold text-2xl mt-5'>Help the beginners to write a better code </h2>
+      <div className='flex flex-col mt-7 gap-2'>
+        <label>Your Code Tip *</label>
+        <textarea className="textarea textarea-bordered border-primary" placeholder="Share your coding tips"></textarea>
+      </div>
+
+      <div className='flex flex-col mt-7 gap-2'>
+        <label className='flex justify-between'>Your Username *
+          <span className='flex items-center gap-2 text-sm'>No Account Needed</span>
+        </label>
+        <input type="text" placeholder="Username" className="input input-bordered w-full border-primary"/>
+      </div>
+
+      
+    </div>
   )
 }
 
