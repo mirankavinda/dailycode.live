c07f954769a804ac71c4d5851f33f0a9c53d89ba
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-20 16:19:50 +0000

    gitignore dbconnection

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const sql = neon("postgresql://linebreak-db_owner:************@ep-young-cherry-a5z1i4lg.us-east-2.aws.neon.tech/linebreak-db?sslmode=require");
-const db = drizzle(sql);
