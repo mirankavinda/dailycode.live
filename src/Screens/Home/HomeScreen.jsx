6574388d6196b117f073695a1e0e88eb46b042b5
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-21 11:08:40 +0000

    trending tab by votes with latest tips

--- src/Screens/Home/HomeScreen.jsx
+++ src/Screens/Home/HomeScreen.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import Tabs from './components/Tabs'
+import { useLocation } from 'react-router-dom'
+import { db } from '../../../utils'
+import { DailyTips } from '../../../utils/schema'
+import { desc } from 'drizzle-orm'
 
-const HomeScreen = () => {
+function HomeScreen() {
 
-  const GetAllCodeTips= () => {
+  const params = useLocation();
+  const [codeTipsList, setCodeTipList] = useState([]);
 
+  useEffect(() => {
+    GetAllCodeTips();
+  },[params])
+
+  const GetAllCodeTips= async() => {
+    const result = await db.select().from(DailyTips)
+    .orderBy(desc(params.hash=='#tranding' || params.hash=='#voted'?DailyTips.vote:DailyTips.id))
+    .limit(20);
+
+    setCodeTipList(result);
   }
 
   return (
