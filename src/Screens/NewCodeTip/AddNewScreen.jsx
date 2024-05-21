3e4f4f8705751d67cd3a405de2d7b18ea4ab9072
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-21 07:44:33 +0000

    typo fixed onChange

--- src/Screens/NewCodeTip/AddNewScreen.jsx
+++ src/Screens/NewCodeTip/AddNewScreen.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Home/components/Header'
-import { ChevronLeft } from 'lucide-react'
-import { Send } from 'lucide-react'
-import { Info } from 'lucide-react'
+import { ChevronLeft, Info, Send } from 'lucide-react'
 
-const AddNewScreen = () => {
+function AddNewScreen() {
+
+  const [codetips,setCodeTip] = useState();
+  const [username,setUsername] = useState();
+
+  const onSavehandler=() => {
+    // logic to save data
+
+  }
+
   return (
     <div>
       <Header/>
@@ -13,17 +20,26 @@ const AddNewScreen = () => {
       <h2 className='font-bold text-2xl mt-5'>Help the beginners to write a better code </h2>
       <div className='flex flex-col mt-7 gap-2'>
         <label>Your Code Tip *</label>
-        <textarea className="textarea textarea-bordered border-primary" placeholder="Share your coding tips"></textarea>
+        <textarea 
+        onChange={(event) => setCodeTip(event.target.value)}
+        className="textarea textarea-bordered border-primary" placeholder="Share your coding tips"></textarea>
       </div>
 
       <div className='flex flex-col mt-7 gap-2'>
         <label className='flex justify-between'>Your Username *
           <span className='flex items-center gap-2 text-sm'><Info className='h-4 w-4'/>No Account Needed</span>
         </label>
-        <input type="text" placeholder="Username" className="input input-bordered w-full border-primary"/>
+        <input type="text" 
+        onChange={(event) => setUsername(event.target.value)}
+        placeholder="Username" className="input input-bordered w-full border-primary"/>
       </div>
 
-      <button className='btn w-full btn-primary mt-7'>Share<Send className='h-4 w-4'/></button>
+      <button className='btn w-full btn-primary mt-7'
+      disabled={!(codetips && username)}
+      onClick={()=>onSavehandler()}
+      >
+        Share<Send className='h-4 w-4'/>
+        </button>
     </div>
   )
 }
