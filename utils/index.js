f576cee89cf2d0d23a0289185fccf901013eb38e
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-25 11:15:31 +0000

    fixed-some-issues

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
+import config from '../config.json';
 
-const sql = neon("postgresql://linebreak-db_owner:dCjQ4XtAbeH9@ep-young-cherry-a5z1i4lg.us-east-2.aws.neon.tech/linebreak-db?sslmode=require");
-export const db = drizzle(sql,{schema});
+const sql = neon(config.databaseUrl);
+export const db = drizzle(sql, { schema });
