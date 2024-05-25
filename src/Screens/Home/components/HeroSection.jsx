15da6f23a6a848da41d55b4f22f8f9c73c259ced
mirankavinda12 <mirankavinda12@gmail.com>
2024-05-25 05:29:35 +0000

    content-change

--- src/Screens/Home/components/HeroSection.jsx
+++ src/Screens/Home/components/HeroSection.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className='my-10 flex flex-col items-center gap-5'>
-        <h2 className='text-3xl font-bold text-center my-5'>Daily Code Tips for Programmers</h2>
+        <h2 className='text-3xl font-bold text-center my-5'>daily code tips for programmers</h2>
         <h2 className='text-center'>
-        Join our community to connect with seasoned programmers, gain valuable insights, and improve your coding skills. 
-        Start your programming journey with support and guidance every step of the way.
+        daily Code offers daily tips, code snippets, and resources to help programmers of all levels improve their skills. 
+        Join us to enhance your coding journey, one day at a time.
         </h2>
     </div>
   )
